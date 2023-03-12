968d57946a8f575e2a0f7c9647e3d3deef4e3f53
UriGood <urielmf.ing@gmail.com>
2023-03-12 16:14:50 -0600

    se añade correccion de cors

--- src/index.js
+++ src/index.js
@@ -2,26 +2,36 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoutes = require("./routes/user");
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8080;
 
 //middleware
+// app.use(function (req, res, next) {
+
+//     res.header('Access-Control-Allow-Origin', "http://localhost:8080");
+//     res.header('Access-Control-Allow-Headers', true);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     next();
+// });
 app.use(express.json());
 app.use('/api', userRoutes);
 
 //routes
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Welcome to my API')
 })
 
 //mongoose connection
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(()=>console.log("connected MongoDB Atlas"))
-    .catch((error)=>console.error(error))
+    .then(() => console.log("connected MongoDB Atlas"))
+    .catch((error) => console.error(error))
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Server listening on port", port);
 });
