cda011f06a6d8632525e91a9113beaa98bd26f96
UriGood <urielmf.ing@gmail.com>
2023-03-13 08:04:08 -0600

    se añade campo de user id

--- src/index.js
+++ src/index.js
@@ -9,14 +9,6 @@ app.use(cors());
 const port = process.env.PORT || 8080;
 
 //middleware
-// app.use(function (req, res, next) {
-
-//     res.header('Access-Control-Allow-Origin', "http://localhost:8080");
-//     res.header('Access-Control-Allow-Headers', true);
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-// });
 app.use(express.json());
 app.use('/api', userRoutes);
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
+    userId:{
+        type: String,
+        required:true
+    },
     userName:{
         type: String,
         required:true
